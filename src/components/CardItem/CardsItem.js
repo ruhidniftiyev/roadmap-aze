ae0dc9cd380c5d3ef55a505ec28b648dc128937f
Khalig Guliyev <halig.guliev@gmail.com>
2022-06-03 13:54:14 +0400

    added cards

--- src/components/CardItem/CardsItem.js
+++ src/components/CardItem/CardsItem.js
@@ -5,14 +5,15 @@ import './CardsItem.css'
 
 const CardsItem = () => {
   const [titles, setTitle] = useState([
-    { title: "FrontEnd", imageUrl: 'https://static.thenounproject.com/png/1442732-200.png'},
-    { title: "BackEnd", imageUrl: 'https://www.pinclipart.com/picdir/big/102-1020519_cloud-server-clipart-transparent-server-png-download.png' },
-    { title: "QA", imageUrl: 'https://cdn3.iconfinder.com/data/icons/web-mobile-development/709/virus_bug_testing_qa_search_dev-512.png' },
-    { title: "Data Analitika", imageUrl: 'https://cdn3.iconfinder.com/data/icons/web-mobile-development/709/virus_bug_testing_qa_search_dev-512.png' },
-    { title: "Mobile", imageUrl: 'https://cdn3.iconfinder.com/data/icons/web-mobile-development/709/virus_bug_testing_qa_search_dev-512.png' },
-    { title: "Sistem Administratoru", imageUrl: 'https://cdn3.iconfinder.com/data/icons/web-mobile-development/709/virus_bug_testing_qa_search_dev-512.png' },
-    { title: "Game", imageUrl: 'https://cdn3.iconfinder.com/data/icons/web-mobile-development/709/virus_bug_testing_qa_search_dev-512.png' },
-    { title: "QA", imageUrl: 'https://cdn3.iconfinder.com/data/icons/web-mobile-development/709/virus_bug_testing_qa_search_dev-512.png' },
+                      { title: "FrontEnd", imageUrl: 'https://cdn-icons.flaticon.com/png/512/5202/premium/5202983.png?token=exp=1654250511~hmac=d11e5450672337a59838d052901e54ae'},
+    { title: "BackEnd", imageUrl: 'https://cdn-icons-png.flaticon.com/512/1119/1119022.png'},
+    { title: "Data Analitika", imageUrl: 'https://cdn-icons-png.flaticon.com/512/1925/1925070.png'},
+    { title: "Mobile", imageUrl: 'https://cdn-icons-png.flaticon.com/512/545/545245.png'},
+    { title: "GameDev", imageUrl: 'https://cdn-icons-png.flaticon.com/512/13/13973.png'},
+    { title: "QA", imageUrl: 'https://cdn-icons-png.flaticon.com/512/554/554200.png' },
+    { title: "Sistem Administratoru", imageUrl: 'https://cdn-icons-png.flaticon.com/512/2082/2082800.png' },
+    { title: "İnformasiya Təhlükəsizliyi ", imageUrl: 'https://cdn-icons-png.flaticon.com/512/2092/2092629.png' },
+    { title: "Cloud Mühəndis", imageUrl: 'https://cdn-icons-png.flaticon.com/512/1793/1793151.png' },
   ]);
 
   return (
